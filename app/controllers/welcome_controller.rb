9197b1eb8ec0c0128f7f92797dfb4a8637659c58
kwaghray <kunalwaghray52@gmail.com>
2014-09-08 16:21:13 +0530

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def aboutus
+  end
+
+  def contactus
+  end
+
+  def specifications
+  end
+  
+  def gallery
+  end
+
+  def feedback
+  end
+
+  def services
+  end
+end
